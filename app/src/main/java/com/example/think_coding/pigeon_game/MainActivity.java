bee8a7e22b813d5bdb0b855b999a8492cd65d5b1
yousinjung <41222835+yousinjung@users.noreply.github.com>
2018-12-15 12:30:01 +0900

    edittext

--- app/src/main/java/com/example/think_coding/pigeon_game/MainActivity.java
+++ app/src/main/java/com/example/think_coding/pigeon_game/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,12 +21,20 @@ public class MainActivity extends AppCompatActivity {
     ViewGroup pigeon_framelayout;
     ImageView pigeon_1;
     ImageView pigeon_2;
+    ViewGroup pigeon_framelayout2;
+    ViewGroup pigeon_framelayout3;
+    ViewGroup pigeon_framelayout4;
     TextView lv_text;
     int lv = 0;
     int add_num = 1;
+    int click_time = 0;
     TextView lv_plus_view;
-
+    TextView function_title;
     Button function_1;
+    Button function_2;
+    EditText name;
+    String save_name;
+
 
 
 
@@ -32,13 +42,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
         setContentView(R.layout.game_playview);
 
-        pigeon_1 = (ImageView) findViewById(R.id.pigeon_1);
+
 
 
 
@@ -46,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         lv_text = (TextView) findViewById(R.id.lv_text);
         lv_plus_view = (TextView) findViewById(R.id.lv_plus_view);
 
-
+        pigeon_1 = (ImageView) findViewById(R.id.pigeon_1);
         pigeon_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,20 +77,33 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }, 500);// 0.5초 정도 딜레이를 준 후 시작
 
-
             }
         });
         main_layout = (ViewGroup) findViewById(R.id.main_relative);
         pigeon_framelayout = (ViewGroup) findViewById(R.id.pigeon_framelayout);
+        pigeon_framelayout2 = (ViewGroup) findViewById(R.id.pigeon_framelayout);
+        pigeon_framelayout3 = (ViewGroup) findViewById(R.id.pigeon_framelayout);
+        pigeon_framelayout4 = (ViewGroup) findViewById(R.id.pigeon_framelayout);
         String[] location = new String[2];
         location[0] = "x";
         location[1] = "y";
 
 
         Movable_Layout_Class new_movable_button = new Movable_Layout_Class(this, main_layout, pigeon_framelayout, location, "scale", false);
+        pigeon_framelayout.setVisibility( View.INVISIBLE);
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) pigeon_framelayout
+                .getLayoutParams();
+        layoutParams.leftMargin = 100;
+        layoutParams.topMargin = 100;
+        pigeon_framelayout.setLayoutParams(layoutParams);
 
-        pigeon_framelayout.setX(10);
-        pigeon_framelayout.setY(10);
+
+        Movable_Layout_Class new_movable_button2 = new Movable_Layout_Class(this, main_layout, pigeon_framelayout2, location, "scale", false);
+        RelativeLayout.LayoutParams layoutParams2 = (RelativeLayout.LayoutParams) pigeon_framelayout2
+                .getLayoutParams();
+        layoutParams2.leftMargin = 50;
+        layoutParams2.topMargin = 100;
+        pigeon_framelayout2.setLayoutParams(layoutParams2);
 
         function_1 = (Button) findViewById(R.id.function_1);
         function_1.setOnClickListener(new View.OnClickListener() {
@@ -92,9 +116,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        function_2 = (Button) findViewById(R.id.function_2);
+        function_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO : click event
+                pigeon_framelayout2.setVisibility(View.VISIBLE);
+                click_time = click_time+1;
+                if(click_time == 1){
+                    pigeon_framelayout2.setVisibility(View.VISIBLE);
+                }else if(click_time ==2){
+                    pigeon_framelayout2.setVisibility(View.INVISIBLE);
+                    pigeon_framelayout3.setVisibility(View.VISIBLE);
+                }else if(click_time ==3){
+                    pigeon_framelayout3.setVisibility(View.INVISIBLE);
+                    pigeon_framelayout4.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
-        function_1.setWidth(500);
 
 
+        function_title = (TextView) findViewById(R.id.function_title);
+        function_title.setTextSize(20);
+
+        name = (EditText) findViewById(R.id.name);
+        save_name = name.getText().toString();
     }//oncreate 끝
 }
